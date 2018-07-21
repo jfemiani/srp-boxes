79558d096374a47d837cf361bad12de5127ff656
John Femiani <femianjc@miamioh.edu>
2018-07-21 16:29:21 -0400

    Minor changes to files as we discussed the way to propare inputs for learning.

--- srp/data/make_trainval.py
+++ srp/data/make_trainval.py
@@ -3,23 +3,23 @@
 Locations of data are specified in config.py
 """
 
-import srp.config 
-import rtree
+import srp.config
+from shapely.strtree import STRtree
+import shapely.geometry as sg
+import fiona
 
 
-# Note -- I may make a class out of this. Logic will remain the same 
+# Note -- I may make a class out of this. Logic will remain the same
 # and I will merge any changes you make in
-def make_trainval(rgb_image_path=None, 
-                  volume_raster_path=None, 
-                  annotations_path=None, 
-                  output_dir=None,
-                  min_separation=None,
-                  C=srp.config
-                  ):
-    
-    nodctest = """
+def make_sample_meta(rgb_image_path=None,
+                     volume_raster_path=None,
+                     annotations_path=None,
+                     output_dir=None,
+                     min_separation=None,
+                     C=srp.config):
+    """
 
-    No 'train' rectangle may overlap a 'val' rectangle. 
+    No 'train' rectangle may overlap a 'val' rectangle.
 
     :param rgb_image_path:
     :param volume_raster_path:
@@ -27,26 +27,21 @@ def make_trainval(rgb_image_path=None,
     :param output_dir:
     :param num_folds: The number of folds to generate
 
-    Examples
-    --------
-    >>> make_trainval(output_dir='data/test', nfolds=5)
-    >>> os.path.isdir('data/test/folds')
-    True
-    >>> os.path.isdir('data/test/folds/1')
-    True
-    >>> os.path.isdir('data/test/folds/5')
-    True
-    >>> os.path.isfile('data/test/folds/1/train.csv')
-    True
+    Example:
 
-    >>> import pandas as pd
-    >>> ds = pd.load_csv('data/test/folds/1/val.csv')
-    >>> ds.columns
-    ['label', 'lon', 'lat', 'box-x', 'box-y', 'box-angle', 'box-length', 'box-width']
+    >>> pos, neg = make_sample_meta(output_dir='data/test')
 
-    The idea is 
-    - `box-cx` is an offset from the sample center
-    - `box-angle` is relative to the _rotated_ sample
+    >>> pos.columns
+    ['lon', 'lat', 'box-x', 'box-y', 'box-angle', 'box-length', 'box-width']
+
+    The idea is
+    - `box-x`. `box-y`' is an offset from the sample center. At this point they will always be 0
+    - `box-angle` is the angle (modulo 90 deg) of the box.
+    - `box-length` is the length of the box
+    - `box-width` is the width of the box
+
+    This function does NOT do data augmentation or split the data.
+    It generates a master set of sample metadata.
 
     """
     rgb_image = rgb_image or C.IMAGE
@@ -54,13 +49,27 @@ def make_trainval(rgb_image_path=None,
     annotations_path = annotations_path or C.ANNOTATIONS
     output_dir = output_dir or C.OUTPUT_DOR
     min_separation = min_separation or C.MIN_SEPARATION
+    num_samples = C.NUM_SAMPLES
 
-    
-    # TODO:
+    # Read in the positive samples
+    pos_samples = [
+        sg.shape(f['geometry']) for f in fiona.open(annotations_path)
+    ]
+    num_pos = len(pos_samples)
+
+    # Build a spatial index to test if a new sample is within `min_separation` of
+    # an existing positive
+    pos_centers = [p.center for p in pos_samples]
+    index = STRtree(pos_center)
+
+    # Generate the (centers of) negative samples
+    num_neg = num_samples - num_pos
+
+    neg_centers = []
+    valid_region = sg.box(*np.r_[np.min(pos_centers, axis=0),
+                                 np.max(pos_centers, axis=0)])
+
     # Read in each shape from the annotations and add a positive sample
     # Make a union of all positive shapes (using shapely)
     # Dilate the union by min_separation
     # randomly select negative points and discard any that fall in the dilated region
-
-
-
