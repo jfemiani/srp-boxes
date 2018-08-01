afebccdf620b4ccbc5d8456a53a44c525fcf0336
John Femiani <femianjc@miamioh.edu>
2018-08-01 15:15:09 -0400

    Added unit tests to arch.py; along with bringing the solver closer to completion.

---
+++ srp/experiment.py
@@ -0,0 +1,14 @@
+
+
+# Use 'python -m srp.experiment data/interim/experiments/00001/config.yml"
+
+#  Read parameters / update config
+
+#  Create the arch
+#  Create the solver
+
+# Pretrain if config says to
+# Train
+
+# Save results somehow
+
