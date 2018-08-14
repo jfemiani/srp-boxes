0305f35360e522ddeded8ae8a7e1121e3d6cf09c
root <liux13@miamioh.edu>
2018-08-14 20:07:36 +0000

    rgblidardataset working

--- srp/model/rgblidar_dataset.py
+++ srp/model/rgblidar_dataset.py
@@ -1,19 +1,10 @@
 import pickle
-import affine
-import pandas as pd
-from srp.config import C
-import sys
-import skimage
+import os
 import numpy as np
-import scipy
-from srp.data.data_provider import DataProvider
+from srp.config import C
+from glob import glob
 from srp.data.orientedboundingbox import OrientedBoundingBox
-from pathlib import Path
-from srp.util import tqdm
-from collections import namedtuple
 from torch.utils.data import Dataset
-from skimage.transform import rotate
-
 from srp.data.generate_patches import Patch
 
 
@@ -23,18 +14,18 @@ class RgbLidarDataset(Dataset):
     This dataset reads in patches that were saved as pickle files. 
     
     """
-    def __init__(self, txt_dir=None):
+    def __init__(self, train_or_test, cache_dir=None):
         """
         This class is design to use in conjunction with the pytorch dataloader class
-        txt_dir: a PosixPath object which specify the path of the .txt which stores train (or test) sample indices.
         
         Attributes:
-        txt_dir: the directory to dataset file, relative to C.TRAIN.SAMPLES.DIR
-        
+        train_or_test: string 'train' or 'test' representing train or test dataset
+        cache_dir: dir to the root of samples, default is C.TRAIN.SAMPLES.DIR. This dataset class load samples
+                   within this directory.
         
         Example:
 
-        >>> trn_data = RgbLidarDataset(trn_txt)
+        >>> trn_data = RgbLidarDataset('train')
         >>> dl = DataLoader(trn_data, batch_size=128, shuffle=True, num_workers=3)
         >>> trn_data.preaugment(num_variants=20)      #generates 20 samples for every record in trn_txt
         >>> X, y = iter(dl).next()
@@ -54,37 +45,43 @@ class RgbLidarDataset(Dataset):
         Please specify epoch length accordingly.
         """
         super().__init__()
+        assert isinstance('train', str)
         
-        self.txt_dir = os.path.join(C.TRAIN.SAMPLES.DIR, txt_dir)
-        with open(txt_dir, 'rb') as f:
-            self.data = f.splitlines
-        self.data = np.loadtxt(txt_dir.as_posix(), dtype=np.uint32, delimiter=',')
-        self.dataframe = pd.DataFrame(data, columns=['label', 'idx'])
-        # self.pre_augmented = False
-        self.cache_dir = Path(C.INT_DATA) / "srp/samples"
+        self.cache_dir = cache_dir or C.TRAIN.SAMPLES.DIR
+        self.txt_dir = os.path.join(C.TRAIN.SAMPLES.DIR, 
+                                    'fold{}'.format(C.TRAIN.SAMPLES.CURRENT_FOLD),
+                                    '{}.txt'.format(train_or_test))
         
-
+        with open(self.txt_dir, 'r') as f:
+            self.dataset = f.read().splitlines()
+        
+        
+    def _load_random_pickle(self, dirname):
+        patches = glob(os.path.join(dirname, '*.pkl'))
+        assert len(patches) == C.TRAIN.AUGMENTATION.VARIATIONS
+        with open(np.random.choice(patches), 'rb') as handle:
+            p = pickle.load(handle)
+        
+        X = np.concatenate((p.rgb, p.volumetric))
+        points = p.obb.points() if p.label else np.zeros((4,2)) 
+        return X, (p.label, points)
+        
+        
     def __getitem__(self, index):
         """Load an augmented version of the sample at `index`
 
         :param index: The index of a sample.
         """
-        rec = self.dataframe.iloc[index]
-        label = "pos" if rec.label == 1 else "neg"
-        sample_name = "s{0:05d}".format(rec.idx)
-        X, y = self._load_random_pickle(self.cache_dir / label / sample_name)
+        rec = self.dataset[index]
+        label = os.path.dirname(rec)
+        print(rec, label)
+        idx = os.path.basename(rec).split('.')[0]
+        
+        dirname = os.path.dirname(C.TRAIN.AUGMENTATION.NAME_PATTERN).format(label=label, name=idx)
+        
+        return self._load_random_pickle(os.path.join(self.cache_dir, dirname))
 
-        return X, y
 
     def __len__(self):
-        return len(self.dataframe)
-
-
-
-def generate_variations():
-    """Generate variations of the original patches. 
-
-    This is used to pre-compute data augmentation for deep learning.
+        return len(self.dataset)
     
-    The number and types of variation are controlled by the configuration file. 
-    """
